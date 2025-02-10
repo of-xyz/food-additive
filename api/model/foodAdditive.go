96f5243a423466e648893b4c20433280afb94ad6
senbei139 <senbei139@gmail.com>
2025-02-10 22:54:20 +0900

    Add DB columns

--- api/model/foodAdditive.go
+++ api/model/foodAdditive.go
@@ -9,10 +9,16 @@ import (
 )
 
 type FoodAdditive struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Purpose     string `json:"purpose"`
-	Description string `json:"description"`
+	ID                 int      `json:"id"`
+	Name               string   `json:"name"`
+	Purpose            string   `json:"purpose"`
+	Description        string   `json:"description"`
+	PermissionEu       string   `json:"permission_eu"`
+	PermissionUs       string   `json:"permission_us"`
+	UsageStatus        string   `json:"usage_status"`
+	RegulationStandard string   `json:"regulation_standard"`
+	Category           int      `json:"category"`
+	Adi                *float64 `json:"adi"`
 }
 
 func (h FoodAdditive) QueryByName(entity string) (*FoodAdditive, error) {
@@ -20,7 +26,7 @@ func (h FoodAdditive) QueryByName(entity string) (*FoodAdditive, error) {
 	row := db_client.QueryRow("SELECT FOOD_ADDITIVE.* FROM FOOD_ADDITIVE INNER JOIN FOOD_ADDITIVE_ALIAS ON FOOD_ADDITIVE.id = FOOD_ADDITIVE_ALIAS.food_additive_id WHERE FOOD_ADDITIVE_ALIAS.alias_name = $1", entity)
 
 	fa := &FoodAdditive{}
-	err := row.Scan(&fa.ID, &fa.Name, &fa.Purpose, &fa.Description)
+	err := row.Scan(&fa.ID, &fa.Name, &fa.Purpose, &fa.Description, &fa.PermissionEu, &fa.PermissionUs, &fa.UsageStatus, &fa.RegulationStandard, &fa.Category, &fa.Adi)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, err
